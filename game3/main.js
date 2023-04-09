4e5dbc404cb9197cff5ccd00dde27759faf20857
ban-masa <bowofvinderre@gmail.com>
2023-04-09 21:19:40 +0900

    added blood

--- game3/main.js
+++ game3/main.js
@@ -7,11 +7,13 @@ var ASSETS = {
     'face_angry' : "./face_angry.png",
     'face_close' : "./face_close.png",
     'face_shame' : "./face_shame.png",
+    'face_eat' : "./face_eat.png",
+    'blood' : "./blood.png",
   },
 };
 
 var SCREEN_WIDTH = 600;
-var SCREEN_HEIGHT = 800;
+var SCREEN_HEIGHT = 600;
 
 phina.define('TitleScene', {
   superClass: 'DisplayScene',
@@ -22,6 +24,205 @@ phina.define('TitleScene', {
     });
     var bg = DisplayElement().addChildTo(this);
     Sprite('title_img').addChildTo(bg).setPosition(this.gridX.center(), this.gridY.center());
+    this.on('pointend', function() {
+      this.exit();
+    });
+  },
+});
+
+/*
+phina.define('TextBox', {
+  superClass: 'DisplayElement',
+  init: function(_parent) {
+    this.superInit();
+    this.parent = _parent;
+  },
+});
+*/
+
+phina.define('CharaImg', {
+  superClass: 'Sprite',
+  init: function(img_name) {
+    this.superInit(img_name);
+    this.textbox = RectangleShape({
+      width: 300,
+      height: 100,
+      stroke: "black",
+      fill: "white",
+      padding: 8,
+      backgroundColor: "gray",
+    }).addChildTo(this).setPosition(0, 150);
+    this.text = Label({
+      text: '▶',
+      fontFamile: "'Monaco', 'Consolas', 'MS 明朝'",
+      fontSize: 22,
+      fill: 'black',
+    }).addChildTo(this).setPosition(0, 150);
+  },
+});
+
+phina.define('Scene1', {
+  superClass: 'DisplayScene',
+  init: function() {
+    this.superInit({
+      width: SCREEN_WIDTH,
+      height: SCREEN_HEIGHT,
+    });
+    this.img_list = [];
+    this.img_list.push(CharaImg('face_normal').addChildTo(this).setPosition(this.gridX.center(), this.gridY.center()).hide());
+    this.img_list.push(CharaImg('face_normal').addChildTo(this).setPosition(this.gridX.center(), this.gridY.center()).hide());
+
+    this.img_list[0].text.text = '▶　お願いってなによわ？';
+    this.img_list[1].text.text = '→『せいら様に噛みつかれたい！』';
+    this.img_list[1].textbox.width = 350;
+
+    this.cnt = 0;
+    this.img_list[0].show();
+
+    this.on('pointend', function() {
+      this.cnt = this.cnt + 1;
+      for (let i = 0; i < this.img_list.length; i++) {
+        this.img_list[i].hide();
+      }
+      if (this.cnt == 0) {
+        this.img_list[0].show();
+      } else if (this.cnt == 1) {
+        this.img_list[1].show();
+      } else {
+        this.exit();
+      }
+    });
+  },
+});
+
+phina.define('Scene2', {
+  superClass: 'DisplayScene',
+  init: function() {
+    this.superInit({
+      width: SCREEN_WIDTH,
+      height: SCREEN_HEIGHT,
+    });
+    this.img_list = [];
+    this.img_list.push(CharaImg('face_angry').addChildTo(this).setPosition(this.gridX.center(), this.gridY.center()).hide());
+
+    this.img_list[0].text.text = '▶　…………は？';
+
+    this.cnt = 0;
+    this.img_list[0].show();
+
+    this.on('pointend', function() {
+      this.exit();
+    });
+  },
+});
+
+phina.define('Scene3', {
+  superClass: 'DisplayScene',
+  init: function() {
+    this.superInit({
+      width: SCREEN_WIDTH,
+      height: SCREEN_HEIGHT,
+    });
+    this.img_list = [];
+    this.img_list.push(CharaImg('face_close').addChildTo(this).setPosition(this.gridX.center(), this.gridY.center()).hide());
+
+    this.img_list[0].text.text = '▶　うーん……';
+
+    this.cnt = 0;
+    this.img_list[0].show();
+
+    this.on('pointend', function() {
+      this.exit();
+    });
+  },
+});
+
+phina.define('Scene4', {
+  superClass: 'DisplayScene',
+  init: function() {
+    this.superInit({
+      width: SCREEN_WIDTH,
+      height: SCREEN_HEIGHT,
+    });
+    this.img_list = [];
+    this.img_list.push(CharaImg('face_shame').addChildTo(this).setPosition(this.gridX.center(), this.gridY.center()).hide());
+
+    this.img_list[0].text.text = '▶　……まったくもう';
+
+    this.cnt = 0;
+    this.img_list[0].show();
+
+    this.on('pointend', function() {
+      this.exit();
+    });
+  },
+});
+
+phina.define('Scene5', {
+  superClass: 'DisplayScene',
+  init: function() {
+    this.superInit({
+      width: SCREEN_WIDTH,
+      height: SCREEN_HEIGHT,
+    });
+    this.backgroundColor = 'black';
+    RectangleShape({
+      width: 300,
+      height: 100,
+      stroke: "black",
+      fill: "white",
+      padding: 8,
+      backgroundColor: "gray",
+    }).addChildTo(this).setPosition(this.gridX.center(), this.gridY.center());
+    Label({
+      text: '▶　じゃあ、いくよわ…',
+      fontFamile: "'Monaco', 'Consolas', 'MS 明朝'",
+      fontSize: 22,
+      fill: 'black',
+    }).addChildTo(this).setPosition(this.gridX.center(), this.gridY.center());
+    
+
+    this.on('pointend', function() {
+      this.exit();
+    });
+  },
+});
+
+phina.define('Scene6', {
+  superClass: 'DisplayScene',
+  init: function() {
+    this.superInit({
+      width: SCREEN_WIDTH,
+      height: SCREEN_HEIGHT,
+    });
+    this.img_list = [];
+    this.img_list.push(CharaImg('face_eat').addChildTo(this).setPosition(this.gridX.center(), this.gridY.center()).hide());
+
+    this.img_list[0].text.text = '▶　あー♡';
+
+    this.cnt = 0;
+    this.img_list[0].show();
+
+    this.on('pointend', function() {
+      this.exit();
+    });
+  },
+});
+
+phina.define('Scene7', {
+  superClass: 'DisplayScene',
+  init: function() {
+    this.superInit({
+      width: SCREEN_WIDTH,
+      height: SCREEN_HEIGHT,
+    });
+    this.img_list = [];
+    this.img_list.push(CharaImg('blood').addChildTo(this).setPosition(this.gridX.center(), this.gridY.center()).hide());
+    this.img_list[0].text.text = 'YOU DIED...';
+
+    this.cnt = 0;
+    this.img_list[0].show();
+
     this.on('pointend', function() {
       this.exit();
     });
@@ -100,14 +301,16 @@ phina.define('MainScene', {
       fill: 'black',
     }).addChildTo(this.face_shame).setPosition(0, 150);
     
+    this.face_eat = Sprite('face_eat').addChildTo(bg).setPosition(this.gridX.center(), this.gridY.center()).hide();
 
     this.cnt = 0;
     this.on('pointend', function() {
-      this.cnt = (this.cnt + 1) % 4;
+      this.cnt = (this.cnt + 1) % 5;
       this.face_normal.hide();
       this.face_angry.hide();
       this.face_close.hide();
       this.face_shame.hide();
+      this.face_eat.hide();
       if (this.cnt == 0) {
         this.face_normal.show();
       } else if (this.cnt == 1) {
@@ -116,6 +319,8 @@ phina.define('MainScene', {
         this.face_close.show();
       } else if (this.cnt == 3) {
         this.face_shame.show();
+      } else if (this.cnt == 4) {
+        this.face_eat.show();
       }
     });
   },
@@ -130,6 +335,48 @@ phina.main(function() {
     startLabel: 'title',
     width: SCREEN_WIDTH,
     height: SCREEN_HEIGHT,
+    scenes: [
+    {
+      className: 'TitleScene',
+      label: 'title',
+      nextLabel: 'scene1',
+    },
+    {
+      className: 'Scene1',
+      label: 'scene1',
+      nextLabel: 'scene2',
+    },
+    {
+      className: 'Scene2',
+      label: 'scene2',
+      nextLabel: 'scene3',
+    },
+    {
+      className: 'Scene3',
+      label: 'scene3',
+      nextLabel: 'scene4',
+    },
+    {
+      className: 'Scene4',
+      label: 'scene4',
+      nextLabel: 'scene5',
+    },
+    {
+      className: 'Scene5',
+      label: 'scene5',
+      nextLabel: 'scene6',
+    },
+    {
+      className: 'Scene6',
+      label: 'scene6',
+      nextLabel: 'scene7',
+    },
+    {
+      className: 'Scene7',
+      label: 'scene7',
+      nextLabel: 'title',
+    },
+    ],
   });
   app.run();
 });
