b683122f5963dc9bdb60ea81dc4974b5ebd6c83f
romiasu <87261081+romiasu@users.noreply.github.com>
2025-09-15 00:45:14 +0900

    ちらつき対策テスト２

--- hiena/script.js
+++ hiena/script.js
@@ -1,4 +1,9 @@
 let isMoving = false;
+let shienaImg;
+let hienaImg;
+let maskImg;
+let handImg;
+let wallImg;
 
 function drawAll (x, y) {
     var canvas1 = document.getElementById('canvas1');
@@ -8,28 +13,18 @@ function drawAll (x, y) {
 
     ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
 
-    var hienaImg = new Image();
-    hienaImg.src = './hiena.png';
     ctx1.globalCompositeOperation = 'source-over';
     ctx1.drawImage(hienaImg, 0, 0, canvas1.width, canvas1.height);
 
-    var maskImg = new Image();
-    maskImg.src = './mask.png';
     ctx1.globalCompositeOperation = 'destination-in';
     ctx1.drawImage(maskImg, x, y, canvas1.width, canvas1.height);
 
-    var handImg = new Image();
-    handImg.src = './hand.png';
     ctx1.globalCompositeOperation = 'source-over';
     ctx1.drawImage(handImg, x, y, canvas1.width, canvas1.height);
 
-    var wallImg = new Image();
-    wallImg.src = './wall2.png';
     ctx1.globalCompositeOperation = 'source-over';
     ctx1.drawImage(wallImg, 0, 0, canvas1.width, canvas1.height);
 
-    var shienaImg = new Image();
-    shienaImg.src = './shiena.png';
     ctx1.globalCompositeOperation = 'destination-over';
     ctx1.drawImage(shienaImg, 0, 0, canvas1.width, canvas1.height);
 
@@ -61,12 +56,14 @@ function first_draw () {
     var canvas2 = document.getElementById('canvas2');
     var ctx2 = canvas2.getContext('2d');
 
-    var shienaImg = new Image();
-    shienaImg.src = './shiena.png';
-    ctx2.globalCompositeOperation = 'source-over';
-    ctx2.drawImage(shienaImg, 0, 0, canvas2.width, canvas2.height);
+    hienaImg = new Image();
+    hienaImg.src = './hiena.png';
+    maskImg = new Image();
+    maskImg.src = './mask.png';
+    handImg = new Image();
+    handImg.src = './hand.png';
 
-    var shienaImg = new Image();
+    shienaImg = new Image();
     shienaImg.onload = function () {
 
         ctx2.globalCompositeOperation = 'source-over';
@@ -77,7 +74,7 @@ function first_draw () {
 
     var drawWall = function () {
         ctx2.globalCompositeOperation = 'source-over';
-        var wallImg = new Image();
+        wallImg = new Image();
         wallImg.onload = function () {
             ctx2.drawImage(wallImg, 0, 0, canvas2.width, canvas2.height);
         };
