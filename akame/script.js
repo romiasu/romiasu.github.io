53d9cc0011ba872fcc247619ad75d2628ff043b3
romiasu <87261081+romiasu@users.noreply.github.com>
2025-09-19 01:30:49 +0900

    added akame

---
+++ akame/script.js
@@ -0,0 +1,85 @@
+let isMoving = false;
+let shienaImg;
+let hienaImg;
+let maskImg;
+let handImg;
+let px = 0;
+let py = 0;
+let scl = 1.0;
+let scope_scale = 1.0;
+
+function drawAll () {
+    var canvas1 = document.getElementById('canvas1');
+    var canvas2 = document.getElementById('canvas2');
+    var ctx1 = canvas1.getContext('2d');
+    var ctx2 = canvas2.getContext('2d');
+
+    ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
+
+    ctx1.globalCompositeOperation = 'source-over';
+    //ctx1.drawImage(hienaImg, 0, 0, canvas1.width, canvas1.height);
+    ctx1.drawImage(hienaImg, (1.0 - scope_scale) * 0.5 * px, (1.0 - scope_scale) * 0.5 * py, canvas1.width * scope_scale, canvas1.height * scope_scale);
+
+    ctx1.globalCompositeOperation = 'destination-in';
+    ctx1.drawImage(maskImg, px - canvas1.width * scl * 0.5, py - canvas1.height * scl * 0.5, canvas1.width * scl, canvas1.height * scl);
+
+    ctx1.globalCompositeOperation = 'source-over';
+    ctx1.drawImage(handImg, px - canvas1.width * scl * 0.5, py - canvas1.height * scl * 0.5, canvas1.width * scl, canvas1.height * scl);
+
+    ctx1.globalCompositeOperation = 'destination-over';
+    ctx1.drawImage(shienaImg, 0, 0, canvas1.width, canvas1.height);
+
+    var dat = ctx1.getImageData(0, 0, canvas1.width, canvas1.height);
+    ctx2.putImageData(dat, 0, 0);
+};
+function init () {
+    var canvas = document.getElementById('canvas2');
+    canvas.addEventListener("mousemove", (e) => {
+        if (!isMoving) return;
+        px = e.offsetX;
+        py = e.offsetY;
+        drawAll();
+    });
+    canvas.addEventListener("mousedown", (e) => {
+        isMoving = true;
+        px = e.offsetX;
+        py = e.offsetY;
+        drawAll();
+    });
+    canvas.addEventListener("mouseup", (e) => {
+        isMoving = false;
+        px = e.offsetX;
+        py = e.offsetY;
+        drawAll();
+    });
+    canvas.addEventListener("wheel", (e) => {
+        e.preventDefault();
+        scl += e.deltaY * -0.001;
+        if (scl > 6.0) scl = 6.0;
+        if (scl < 1.0) scl = 1.0;
+        scope_scale = (scl - 1.0) / 5.0 * 0.0 + 1.0;
+        drawAll();
+    }, {passive: false});
+};
+function first_draw () {
+    var canvas2 = document.getElementById('canvas2');
+    var ctx2 = canvas2.getContext('2d');
+
+    hienaImg = new Image();
+    hienaImg.src = './akame2.png';
+    maskImg = new Image();
+    maskImg.src = './mask2.png';
+    handImg = new Image();
+    handImg.src = './hand2.png';
+
+    shienaImg = new Image();
+    shienaImg.onload = function () {
+
+        ctx2.globalCompositeOperation = 'source-over';
+        ctx2.drawImage(shienaImg, 0, 0, canvas2.width, canvas2.height);
+    };
+    shienaImg.src = './akame1.png';
+};
+
+init();
+first_draw();
